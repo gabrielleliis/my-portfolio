9adaa1a41137154d606ab89a5e42d23dd350d645
gabrielleliis <lelisgabrielcs@gmail.com>
2025-10-15 12:28:37 -0300

    feat: configs material ui theme, css basiline

---
+++ src/theme.ts
@@ -0,0 +1,20 @@
+import { createTheme, responsiveFontSizes } from '@mui/material/styles';
+import { green, purple } from '@mui/material/colors';
+
+let theme = createTheme({
+  palette: {
+    primary: {
+      main: purple[500],
+    },
+    secondary: {
+      main: green[500],
+    },
+  },
+  typography: {
+    fontFamily: "Montserrat, sans-serif",
+  }
+});
+
+theme = responsiveFontSizes (theme);
+
+export default theme;
